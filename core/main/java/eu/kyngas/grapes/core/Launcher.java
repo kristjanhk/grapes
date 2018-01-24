57d3059f4ec208c2b001409df549108a44fb1c0f
kristjanhk <kristjankyngas@outlook.com>
2018-01-24 23:32:16 +0200

    Init

---
+++ core/main/java/eu/kyngas/grapes/core/Launcher.java
@@ -0,0 +1,11 @@
+package eu.kyngas.grapes.core;
+
+/**
+ * @author <a href="https://github.com/kristjanhk">Kristjan Hendrik Küngas</a>
+ */
+public class Launcher {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
