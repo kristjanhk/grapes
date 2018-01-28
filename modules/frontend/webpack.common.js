40f0df9bf883a1837c5bf1c18e515ad80ee5a51c
kristjanhk <kristjankyngas@outlook.com>
2018-01-28 13:46:50 +0200

    (#11) Webpack and tsconfig path resolving

--- modules/frontend/webpack.common.js
+++ modules/frontend/webpack.common.js
@@ -1,34 +1,29 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const paths = {
-  nodeModules: path.resolve(__dirname, './node_modules'),
-  src: path.resolve(__dirname, './src'),
-  dist: path.resolve(__dirname, './main/resources/static/dist'),
-  css: path.resolve(__dirname, './assets/static/css'),
-  img: path.resolve(__dirname, './assets/static/img'),
-  font: path.resolve(__dirname, './assets/static/font')
-};
+const src = path.resolve(__dirname, './src');
 
 module.exports = {
   cache: true,
-  context: paths.src,
-  entry: paths.src + '/index.tsx',
+  context: src,
+  entry: path.join(src, 'index.tsx'),
   output: {
     publicPath: '/',
-    path: paths.dist
+    path: path.resolve(__dirname, './main/resources/static/dist')
   },
   resolve: {
     alias: {
-      css: paths.css,
-      img: paths.img,
-      font: paths.font
+      css: path.join(src, 'css'),
+      img: path.join(src, 'images'),
+      font: path.join(src, 'fonts'),
+      music: path.join(src, 'music'),
+      components: path.join(src, 'components'),
     },
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: paths.src + '/index.html'
+      template: path.join(src, 'index.html'),
     }),
   ],
   module: {
