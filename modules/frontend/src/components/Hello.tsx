c032f0a733a0e5c3c19d85a691cfd54c2523bade
kristjanhk <kristjankyngas@outlook.com>
2018-02-06 21:59:22 +0200

    (#18) Audio streaming in react

--- modules/frontend/src/components/Hello.tsx
+++ modules/frontend/src/components/Hello.tsx
@@ -1,3 +1,4 @@
+import {Howl} from "howler";
 import * as React from "react";
 
 export interface IHelloProps {
@@ -6,6 +7,15 @@ export interface IHelloProps {
 }
 
 export class Hello extends React.Component<IHelloProps, {}> {
+  constructor(props: IHelloProps) {
+    super(props);
+    const howler = new Howl({
+      format: ["mp3"],
+      html5: true,
+      src: "https://kyngas.eu/radio/stream",
+    });
+    howler.play();
+  }
   public render() {
     return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
   }
